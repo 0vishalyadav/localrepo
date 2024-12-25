26db6cba221635547cfb17338d8d55fbadee39ce
0vishalyadav <vishalyadavf083@gmail.com>
2024-12-26 00:16:46 +0530

    looptestprogram

---
+++ javalooptest.java
@@ -0,0 +1,11 @@
+public class javalooptest {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<5) {
+            System.out.println("hell0 world");
+            i++;
+        }
+    }
+
+    
+}
